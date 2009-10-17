68bf93f353f144d03c085c9d51c006820439db0e
dpranke <dpranke@feea040c-a742-11de-b2a4-832221a088ed>
2009-10-17 02:19:22 +0000

    Merge home:/src/json_db

--- trunk/python/json_db_test.py
+++ trunk/python/json_db_test.py
@@ -19,6 +19,47 @@ from json_db import *
 import unittest
 import sys
 
+def tableOne():
+  return Table({"rows": [[1, 2], [3, 4]], 
+                "columns": ["a", "b"], 
+                "primary key":"a"})
+
+def tableTwo():
+  return Table({"rows": [[1, 2], [3, 4]]})
+
+def tableEmp():
+  return Table({"name": "emp", "columns":["empno", "dept"], 
+                "rows":[[1, 1],[2, 2],[3, 3]],
+                "primary key": "empno"})
+
+def tableThree():
+  return Table({"columns": ["a", "b"], 
+                "rows":   [[1, 2], [2, 3], [3, 4]]})
+
+def tableFour():
+  return Table({"columns": ["a", "b"], 
+                "rows":   [[1, 2], [2, 3], [5, 6]]})
+
+def tableFive():
+  return Table({"columns": ["a", "b"], 
+                "rows":   [[1, 2], [2, 3], [3, 4], [5, 6]]})
+
+def tableSix():
+  return Table({"columns": ["a", "b"], 
+                "rows":   [[5, 6]]}) 
+
+def jsonOne():
+  return ('{"kind": "table", "version": 1, "columns": ["a", "b"], '
+          '"primary key": "a", "rows": [[1, 2], [3, 4]]}')
+
+def jsonTwo():
+  return ('{"kind": "table", "version": 1, "columns": ["c0", "c1"], '
+          '"rows": [[1, 2], [3, 4]]}')
+
+def jsonEmp():
+  return ('{"primary key": "empno", "rows": [[1], [2], [3]], '
+          '"version": 1, "columns": ["empno"], "name": "emp"}')
+ 
 class MockStream(object):
   def __init__(self, input = None):
     self.writes = []
@@ -38,17 +79,24 @@ class MockStream(object):
   def getWrites(self):
     return self.writes
 
-class defTests(unittest.TestCase):
+class FunctionTests(unittest.TestCase):
   def testTableFromCSV(self):
     t = TableFromCSV(["a,b","1,2"], True)
     self.assertEqual(t, Table({"columns":["a","b"],"rows":[["1","2"]]}))
+    t = TableFromCSV(["1,2","3,4"], False)
+    self.assertEqual(t, Table({"columns":["c0", "c1"],
+        "rows": [["1", "2"], ["3", "4"]]}))
+    t = TableFromCSV(["1,2","3,4"], False, ["a", "b"])
+    self.assertEqual(t, Table({"columns":["a", "b"],
+        "rows": [["1", "2"], ["3", "4"]]}))
+
 
   def testTableToCSV(self):
-    t = Table({"columns":["a","b"],"rows":[["1","2"]]})
+    t = Table({"columns":["a","b"],"rows":[["1",None]]})
     stdout = MockStream()
-    TableToCSV(stdout, t)
-    self.assertEqual(stdout.getWrites(), ['a,b\r\n','1,2\r\n'])
-
+    TableToCSV(stdout, t, "null")
+    self.assertEqual(stdout.getWrites(), ['a,b\r\n','1,null\r\n'])
+ 
 class CLITests(unittest.TestCase):
   def testMain(self):
     stdin = MockStream()
@@ -58,65 +106,76 @@ class CLITests(unittest.TestCase):
     Main(None, ["-n", "-t", "../test.jsondb"], stdin, stdout, stderr)
     Main(None, ["--verbose", "-t", "../test.jsondb"], stdin, stdout, stderr)
 
+class DatabaseTests(unittest.TestCase):
+  """Test json_db.Database."""
+
+  def testConstructors(self):
+    db = Database()
+    self.assertEqual(db, 
+        Database('{"kind":"database","version":1,"tables":{}}'))
+    db = Database('{"tables":{"a": {"columns": ["a", "b"], '
+                  '"rows": [[1, 2], [3, 4]], "primary key": "a"}}}')
+    self.assertEqual(db['a'], tableOne())
+    self.assertEqual(str(db), '{"kind": "database", "version": 1, '
+        '"tables": {"a": {"kind": "table", "version": 1, '
+        '"columns": ["a", "b"], "primary key": "a", '
+        '"rows": [[1, 2], [3, 4]]}}}')
+    self.assertEqual(repr(db), '{"kind": "database", "version": 1, '
+        '"tables": {"a": {"kind": "table", "version": 1, '
+        '"columns": ["a", "b"], "primary key": "a", '
+        '"rows": [[1, 2], [3, 4]]}}}')
+
+  def testNameAndComment(self):
+    db = Database()
+    self.assertEqual(db.name(), None)
+    self.assertEqual(db.comment(), None)
+    db.setName("db")
+    db.setComment("test db")
+    self.assertEqual(db.name(), "db")
+    self.assertEqual(db.comment(), "test db")
+
+  def testDumps(self):
+    db = Database({"name": "db", "comment": "test", 
+        "tables": {"a": {"rows":[[0,1,2],[3,4,5]]}}})
+    self.assertEqual(db._dumps(False, False),
+        '{"kind": "database", "version": 1, "name": "db", "comment": "test", '
+        '"tables": {"a": {"kind": "table", "version": 1, "columns": '
+        '["c0", "c1", "c2"], "rows": []}}}')
+    self.assertEqual(db._dumps(True, False),
+        '{"kind": "database", "version": 1, "name": "db", "comment": "test", '
+        '"tables": {"a": {"kind": "table", "version": 1, "columns": '
+        '["c0", "c1", "c2"], "rows": [[0, 1, 2], [3, 4, 5]]}}}')
+    pretty = """{ "kind": "database",
+  "version": 1,
+  "name": "db",
+  "comment": "test",
+  "tables": {
+    "a": {
+      "kind": "table",
+      "version": 1,
+      "columns": ["c0", "c1", "c2"],
+      "row_count": 2,
+      "rows": [[0, 1, 2],
+               [3, 4, 5]]
+    }}}"""
+    s = db._dumps(True, True)
+    self.assertEqual(db._dumps(True, True), pretty)
+
+  def testEq(self):
+    db = Database()
+    db['a'] = tableOne()
+    db['b'] = tableTwo()
+    db2 = Database()
+    db2['a'] = tableOne()
+    db2['b'] = tableTwo()
+    # self.assertEqual(db, db2)
+
 class TableTests(unittest.TestCase):
   """Do some basic testing for json_db."""
-  def tableOne(self):
-    return Table({"rows": [[1, 2], [3, 4]], 
-                  "columns": ["a", "b"], 
-                  "primary key":"a"})
-
-  def tableTwo(self):
-    return Table({"rows": [[1, 2], [3, 4]]})
-
-  def tableEmp(self):
-    return Table({"name": "emp", "columns":["empno", "dept"], 
-                  "rows":[[1, 1],[2, 2],[3, 3]],
-                  "primary key": "empno"})
-
-  def tableThree(self):
-    return Table({"columns": ["a", "b"], 
-                  "rows":   [
-                             [ 1,   2 ],
-                             [ 2,   3 ],
-                             [ 3,   4 ],
-                            ]})
-
-  def tableFour(self):
-    return Table({"columns": ["a", "b"], 
-                  "rows":   [
-                             [ 1,   2 ],
-                             [ 2,   3 ],
-                             [ 5,   6 ],
-                            ]})
-
-  def tableFive(self):
-    return Table({"columns": ["a", "b"], 
-                  "rows":   [
-                             [ 1,   2 ],
-                             [ 2,   3 ],
-                             [ 3,   4 ],
-                             [ 5,   6 ],
-                            ]})
-
-  def tableSix(self):
-    return Table({"columns": ["a", "b"], 
-                  "rows":   [
-                             [ 5,   6 ],
-                            ]})
-
-
-  def jsonOne(self):
-     return '{"kind": "table", "version": 1, "columns": ["a", "b"], "primary key": "a", "rows": [[1, 2], [3, 4]]}'
-
-  def jsonTwo(self):
-    return '{"kind": "table", "version": 1, "columns": ["c0", "c1"], "rows": [[1, 2], [3, 4]]}'
-
-  def jsonEmp(self):
-    return '{"primary key": "empno", "rows": [[1], [2], [3]], "version": 1, "columns": ["empno"], "name": "emp"}'
-             
+            
   def testConstructors(self):
-    self.assertEqual(str(self.tableOne()), """{"kind": "table", "version": 1, "columns": ["a", "b"], "primary key": "a", "rows": [[1, 2], [3, 4]]}""")
-    self.assertEqual(repr(Table({"rows": [[1, 2], [3, 4]]})), self.jsonTwo())
+    self.assertEqual(str(tableOne()), """{"kind": "table", "version": 1, "columns": ["a", "b"], "primary key": "a", "rows": [[1, 2], [3, 4]]}""")
+    self.assertEqual(repr(Table({"rows": [[1, 2], [3, 4]]})), jsonTwo())
     self.assertEqual(Table({"rows": [[1,2],[3,4]], "version": 1}),
                      Table({"rows": [[1,2],[3,4]]}))
     
@@ -164,66 +223,66 @@ class TableTests(unittest.TestCase):
     self.assertEqual(t, None)
 
   def testRepr(self):
-    self.assertEqual(repr(self.tableOne()), self.jsonOne())
+    self.assertEqual(repr(tableOne()), jsonOne())
 
   def testLen(self):
-    self.assertEqual(len(self.tableOne()), 2)
+    self.assertEqual(len(tableOne()), 2)
 
   def testIter(self):
     s = ""
-    for r in self.tableOne():
+    for r in tableOne():
       s = s + ",".join([str(c) for c in r]) 
       s = s + "\n"  
     self.assertEqual(s, "1,2\n3,4\n")
 
   def testName(self):
-    self.assertEqual(self.tableOne().name(), None)
-    self.assertEqual(self.tableEmp().name(), "emp")
+    self.assertEqual(tableOne().name(), None)
+    self.assertEqual(tableEmp().name(), "emp")
 
   def testColumns(self):
-    self.assertEqual(self.tableOne().columns(), ["a","b"])
-    self.assertEqual(self.tableTwo().columns(), ["c0", "c1"])
+    self.assertEqual(tableOne().columns(), ["a","b"])
+    self.assertEqual(tableTwo().columns(), ["c0", "c1"])
 
   def testRowAsList(self):
-    self.assertEqual(self.tableOne().rowAsList('1'), [1,2])
+    self.assertEqual(tableOne().rowAsList('1'), [1,2])
 
   def testRows(self):
-    self.assertEqual(self.tableOne().rows(), 
+    self.assertEqual(tableOne().rows(), 
                      [[1, 2],[3, 4]])
 
   def testRow(self):
-    self.assertEqual(self.tableOne().row('1'), Row({"a": 1, "b": 2})) 
-    self.assertEqual(self.tableOne().row(1), Row({"a": 1, "b": 2})) 
+    self.assertEqual(tableOne().row('1'), Row({"a": 1, "b": 2})) 
+    self.assertEqual(tableOne().row(1), Row({"a": 1, "b": 2})) 
 
   def testRowByIndex(self):
-    self.assertEqual(self.tableOne().rowByIndex(1), Row({"a": 3, "b": 4}))
+    self.assertEqual(tableOne().rowByIndex(1), Row({"a": 3, "b": 4}))
 
   def testRowByKey(self):
-    self.assertEqual(self.tableOne().rowByKey('1'), Row({"a": 1, "b": 2}))
-    self.assertEqual(self.tableOne().rowByKey(1), Row({"a": 1, "b": 2}))
+    self.assertEqual(tableOne().rowByKey('1'), Row({"a": 1, "b": 2}))
+    self.assertEqual(tableOne().rowByKey(1), Row({"a": 1, "b": 2}))
 
   def testRename(self):
-    self.assertEqual(self.tableOne().rename({"a":"c0", "b": "c1"}),
+    self.assertEqual(tableOne().rename({"a":"c0", "b": "c1"}),
                      Table({"primary key": "c0",
                             "rows": [[1, 2], [3, 4]], 
                             "columns":["c0", "c1"]}))
-    self.assertEqual(self.tableOne().rename({"b":"c1"}),
+    self.assertEqual(tableOne().rename({"b":"c1"}),
                      Table({"primary key": "a",
                             "rows": [[1, 2], [3, 4]], 
                             "columns":["a", "c1"]}))
 
 
   def testProject(self):
-    self.assertEqual(self.tableOne().project(["a"]),
+    self.assertEqual(tableOne().project(["a"]),
                      Table({"primary key": "a", 
                             "rows": [[1], [3]], 
                             "columns": ["a"]})) 
-    self.assertEqual(self.tableOne().project(["b"]),
+    self.assertEqual(tableOne().project(["b"]),
                      Table({"rows": [[2], [4]], 
                             "columns": ["b"]})) 
                      
   def testRestrict(self):
-    self.assertEqual(self.tableOne().restrict( lambda x : x.a == 1 ),
+    self.assertEqual(tableOne().restrict( lambda x : x.a == 1 ),
                      Table({"primary key": "a", 
                             "rows": [[1, 2]], 
                             "columns": ["a", "b"]}))
@@ -233,21 +292,21 @@ class TableTests(unittest.TestCase):
       r.b = int(r.a) * 3
       return r
 
-    self.assertEqual(self.tableOne().update(fn), 
+    self.assertEqual(tableOne().update(fn), 
                      Table({"columns": ["a", "b"],
                             "rows": [[1, 3], [3, 9]]}))
 
   def testExtend(self):
-    self.assertEqual(self.tableOne().extend( 
+    self.assertEqual(tableOne().extend( 
         lambda x: Row({"c": int(x.a) + int(x.b), "d": int(x.a) - int(x.b)})),
         Table({"columns": ["a", "b", "c", "d"],
                "rows":   [[1, 2, 3, -1],
                           [3, 4, 7, -1]]}))
 
   def test__GetItem__(self):
-    self.assertEqual(self.tableOne()[1], Row({"a": 1, "b": 2}))
-    self.assertEqual(self.tableOne()['1'], Row({"a": 1, "b": 2}))
-    self.assertEqual(self.tableOne()[0], Row({"a": 1, "b": 2}))
+    self.assertEqual(tableOne()[1], Row({"a": 1, "b": 2}))
+    self.assertEqual(tableOne()['1'], Row({"a": 1, "b": 2}))
+    self.assertEqual(tableOne()[0], Row({"a": 1, "b": 2}))
 
   def testOrderBy(self):
     t1 = Table({"columns":["a", "b"],
@@ -348,10 +407,10 @@ class TableTests(unittest.TestCase):
     self.assertEqual(t, None)
 
   def testUnion(self):
-    self.assertEqual(self.tableThree().union(self.tableFour()),
-                     self.tableFive())
+    self.assertEqual(tableThree().union(tableFour()),
+                     tableFive())
 
-    t = self.tableEmp().union(Table({"name": "emp", 
+    t = tableEmp().union(Table({"name": "emp", 
        "columns":["empno", "dept"], "rows": [[1,1],[5,5]], 
        "primary key": "empno"}))
     self.assertEqual(t, Table({"name": "emp", 
@@ -361,7 +420,7 @@ class TableTests(unittest.TestCase):
     t = None
     msg = ""
     try:
-      t = self.tableEmp().union(Table({"name": "emp", 
+      t = tableEmp().union(Table({"name": "emp", 
        "columns":["empno", "dept"], "rows": [[1,3],[5,5]], 
        "primary key": "empno"}))
     except ValueError, e:
@@ -371,12 +430,12 @@ class TableTests(unittest.TestCase):
     self.assertEqual(msg, "duplicate primary key \"1\" in union")
 
   def testIntersect(self):
-    self.assertEqual(self.tableFive().intersect(self.tableThree()),
-                     self.tableThree())
+    self.assertEqual(tableFive().intersect(tableThree()),
+                     tableThree())
 
   def testMinus(self):
-    self.assertEqual(self.tableFive().minus(self.tableThree()),
-                     self.tableSix())
+    self.assertEqual(tableFive().minus(tableThree()),
+                     tableSix())
  
   def testSummarize(self):
     t1 = Table({"columns" : ["a", "b", "c"],
